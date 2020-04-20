4d750d1ef8fc8113df9b4ddbcda5816d12a91330
Kumar <s.ashok.kumar@siemens.com>
2020-04-21 01:40:01 +0530

    fist commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+            .HasData(
+                new Value{Id=1,Name="Ashok"},
+                new Value{Id=2,Name="Ashok1"},
+                new Value{Id=3,Name="Ashok12"},
+                new Value{Id=4,Name="Ashok123"},
+                new Value{Id=5,Name="Ashok1234"}
+            );
+        }
+    }
+}
